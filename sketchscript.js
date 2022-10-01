c3845de93d525574e47cfc3f98e86e98c1639e59
AWSueme <weirdwolfman@gmail.com>
2022-10-01 15:30:17 -0700

    Add event listener and shader to each pixel

--- sketchscript.js
+++ sketchscript.js
@@ -4,9 +4,12 @@ for(let i=0; i < 100; i++){
     let pixel=document.createElement('div');
     pixel.setAttribute("class", "pixel");
     screen.appendChild(pixel);
-    pixel.addEventListener("hover", shade);
+    pixel.addEventListener("mouseover", shade);
 }
 
+screen.style.backgroundColor=blue;
+
 function shade(e){
-    e.target.setAttribute
+    let pix = e.target;
+    pix.style.backgroundColor="blue";
 }
