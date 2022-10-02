821e1719911dd42456a937114dc55fff4ccaaa16
AWSueme <weirdwolfman@gmail.com>
2022-10-01 23:12:29 -0700

    Add incremented shading to each pixel

--- sketchscript.js
+++ sketchscript.js
@@ -1,15 +1,15 @@
 let screen=document.querySelector("#screen");
 
-for(let i=0; i < 100; i++){
+for(let i=0; i < 900; i++){
     let pixel=document.createElement('div');
     pixel.setAttribute("class", "pixel");
     screen.appendChild(pixel);
     pixel.addEventListener("mouseover", shade);
 }
 
-screen.style.backgroundColor=blue;
-
 function shade(e){
-    let pix = e.target;
-    pix.style.backgroundColor="blue";
+    let p = Number(e.target.style.opacity);
+    p = p + 0.1;
+    console.log(p);
+    e.target.style.opacity = p;
 }
