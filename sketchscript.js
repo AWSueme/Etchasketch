8245c44780958d151f9465ae72a0d68182a6f7a8
AWSueme <weirdwolfman@gmail.com>
2022-10-02 12:28:15 -0700

    Added reset button to clear screen with functionality

--- sketchscript.js
+++ sketchscript.js
@@ -1,11 +1,26 @@
 let screen=document.querySelector("#screen");
 
-for(let i=0; i < 900; i++){
-    let pixel=document.createElement('div');
-    pixel.setAttribute("class", "pixel");
-    screen.appendChild(pixel);
-    pixel.addEventListener("mouseover", shade);
+screenrender();
+
+reset=document.querySelector("#reset");
+reset.addEventListener("click", screenrender)
+
+function screenrender(){
+
+    while (screen.firstChild) {
+        screen.removeChild(screen.firstChild);
+    }
+
+    for(let i=0; i < 900; i++){
+        let pixel=document.createElement('div');
+        pixel.setAttribute("class", "pixel");
+        screen.appendChild(pixel);
+        pixel.addEventListener("mouseover", shade);
+    }
+    
 }
+
+
 
 function shade(e){
     let p = Number(e.target.style.opacity);
