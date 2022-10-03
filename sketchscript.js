d4adc58db45b17012d2a91711a6c48c901cb9496
AWSueme <weirdwolfman@gmail.com>
2022-10-02 21:47:43 -0700

    Added random color mode.
    connected to Random button.

--- sketchscript.js
+++ sketchscript.js
@@ -6,8 +6,9 @@ let pen=document.querySelector("#pencolor");
 pen.addEventListener("change", colorchange);
 reset.addEventListener("click", screenrender);
 rainbow.addEventListener("click", screenrender);
-random.addEventListener("click", screenrender);
+random.addEventListener("click", rainbowrender);
 let colour;
+let rando = false;
 
 screenrender();
 
@@ -17,13 +18,15 @@ function screenrender(){
         screen.removeChild(screen.firstChild);
     }
 
-    for(let i=0; i < 900; i++){
+    for(let i=0; i < 3600; i++){
         let pixel=document.createElement('div');
         pixel.setAttribute("class", "pixel");
         screen.appendChild(pixel);
         pixel.addEventListener("mouseover", shade);
         
-        
+        if(rando){
+            colour="#" + Math.floor(Math.random()*16777215).toString(16);
+        }
         
         pixel.style.backgroundColor=colour;
     }
@@ -32,16 +35,18 @@ function screenrender(){
 
 function colorchange(e){
     colour= e.target.value;
+    rando=false;
     screenrender();
 }
 
 function rainbowrender(){
+    rando=true;
     screenrender();
 
 }
 
 function shade(e){
     let p = Number(e.target.style.opacity);
-    p = p + 0.1;
+    p = p + 0.25;
     e.target.style.opacity = p;
 }
